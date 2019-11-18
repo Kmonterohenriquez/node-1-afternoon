00ab63c0e5131abde5cb06eb0a8befb5ae7ba361
Kevin Montero <kevinwilding1997@gmail.com>
2019-11-18 14:40:34 -0700

    Assignment Done

--- server/index.js
+++ server/index.js
@@ -5,16 +5,6 @@ const getProduct = require('./getProduct');
 
 const app = express();
 
-
-
-// app.use(express.json());
-
-
-// app.get('/api/products/:id', (red, res) => {
-//     console.log(res)
-//     res.status(200).send(products)
-// });
-
 app.get('/api/products', getProducts);
 app.get('/api/product/:id', getProduct);
 
